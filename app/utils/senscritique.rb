99a7fceb2b9e654f68f515fdaa8b830fe0e5c738
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-10-29 19:24:34 +0100

    fix: removed a forgotten 'puts'

--- app/utils/senscritique.rb
+++ app/utils/senscritique.rb
@@ -15,7 +15,6 @@ class Senscritique
     updated = 0
     created = 0
     last_page = @@CONFIG["debug"]["page_limit"] == 0 ? get_last_page(type_fr) : @@CONFIG["debug"]["page_limit"]
-    puts last_page
 
     (1..last_page).each do | page_number |
       puts "----- #{type_en.capitalize} - page #{page_number}/#{last_page} -----"
